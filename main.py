6d84d73a61556215d8e4a295560dcf296479a373
Kumar Nishant <nkumar@demandbase.com>
2024-01-14 02:16:49 +0000

    updating main script for streaming data

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 # This is a sample Python script for testing
 import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
 from dataSampler.SampleSize import *
 from dataSampler.SelectSampleData import *
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
 
-    # Example usage for selecting sample number :
-    print('Example usage for selecting sample number \n')
-    population_size = 100000
-    ps = PopulationBasedSampleSize(population_size)
-    print(f'The sample size is :{ps.calculate_sample_size()} \n' )
+    # Reading the data
+    
+    for idx,df_data in tqdm(enumerate(pd.read_csv('data/exp_ln_data.csv',chunksize=10000000))):
+        print('Reading the data...')
+    
+        population_size = len(df_data)
+        # print(df_data.head())
+        # print(df_data.shape)
+        
+        print('Formatting the data...')
+        data_format_list = list(zip(df_data['desc_md5_cs'],df_data['doc_occur_count']))
+        # print(f'Formatted data is :\n {data_format_list[:10]}\n')
+        
+        # Example usage for selecting sample number :
+        print('Selecting sample size...')
+        ps = PopulationBasedSampleSize(population_size)
+        sample_k = ps.calculate_sample_size()
+        # print(f'The sample size is :{sample_k}\n' )
 
-    # Example usage for selecting sample data :
-    df_data = pd.read_csv('data/sample.csv')
-    print('Example usage for selecting sample data \n')
-    sample_list = []
-    for idx, data in df_data.iterrows():
-        sample_list.append((data['desc_md5_cs'], data['doc_occur_count']))
-    ss = StatsSampling(sample_list, 20)
-    print(f'The sample data is :\n {ss.run_sampler()} \n' )
-
+        # Example usage for selecting sample data :
+        
+        print('Selecting sample data...')
+        ss = StatsSampling(data_format_list, sample_k)
+        sampled_data = ss.run_sampler()
+        
+        df_sample = pd.DataFrame(sampled_data, columns =['desc_md5_cs','doc_occur_count'])
+        # print(f'The sample data is :\n' )
+        # print(f'Size of sampled data : {df_sample.shape}')
+        print('Saving sample... ')
+        output_file = f'data/sampleData/sample_data_{str(idx)}.csv' 
+        df_sample.to_csv(output_file,index=False)
